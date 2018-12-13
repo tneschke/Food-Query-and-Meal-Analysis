65ec7ce10c3168fa0c3aaccbb2e8306172d15656
Kiara Mutschler <43933594+kjmutsch@users.noreply.github.com>
2018-12-12 20:18:04 -0600

    Update FoodTableSample.java

--- FoodTableSample.java
+++ FoodTableSample.java
@@ -17,19 +17,30 @@
 package application;
 import java.util.List;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  * This class creates the tables for presenting the food list and meal analysis
@@ -57,8 +68,9 @@ public class FoodTableSample {
 	        List<FoodItem> foodList = foods.getAllFoodItems();
 	        for(FoodItem food : foodList) {
 	        	TableItem tableItem = new TableItem(food.getName(), String.valueOf(food.getNutrientValue("calories")),
-	        			String.valueOf(food.getNutrientValue("fat")),String.valueOf(food.getNutrientValue("fiber")),
-	        			String.valueOf(food.getNutrientValue("protein")),String.valueOf(food.getNutrientValue("carbohydrate")));
+	        			String.valueOf(food.getNutrientValue("fat")), String.valueOf(food.getNutrientValue("carbohydrate")), 
+	        			String.valueOf(food.getNutrientValue("fiber")),
+	        			String.valueOf(food.getNutrientValue("protein")));
 	        	data.add(tableItem);	
 	        }
 	        hb = new HBox();
@@ -71,8 +83,9 @@ public class FoodTableSample {
     	
         for(FoodItem food : foodList) {
         	TableItem tableItem = new TableItem(food.getName(), String.valueOf(food.getNutrientValue("calories")),
-        			String.valueOf(food.getNutrientValue("fat")),String.valueOf(food.getNutrientValue("fiber")),
-        			String.valueOf(food.getNutrientValue("protein")),String.valueOf(food.getNutrientValue("carbohydrate")));
+        			String.valueOf(food.getNutrientValue("fat")),String.valueOf(food.getNutrientValue("carbohydrate")),
+        			String.valueOf(food.getNutrientValue("fiber")),
+        			String.valueOf(food.getNutrientValue("protein")));
         	data.add(tableItem);	
         }
         table.setItems(data);
@@ -117,8 +130,29 @@ public class FoodTableSample {
 		proteinCol.setMinWidth(100);
 		proteinCol.setCellValueFactory(new PropertyValueFactory<>("protein"));
 
+		TableColumn<TableItem, String> addItemCol = new TableColumn<>("Add Item");
+		addItemCol.setMinWidth(100);
+		addItemCol.setCellValueFactory(new PropertyValueFactory<>("add item"));
+
+		// creates a column where user can remove or add fooditem in the selected row
+		TableColumn<TableItem, Boolean> actionCol = new TableColumn<>("Add/Delete");
+		actionCol.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<TableItem, Boolean>, ObservableValue<Boolean>>() {
+					@Override
+					public ObservableValue<Boolean> call(TableColumn.CellDataFeatures<TableItem, Boolean> features) {
+						return new SimpleBooleanProperty(features.getValue() != null);
+					}
+				});
+
+		actionCol.setCellFactory(new Callback<TableColumn<TableItem, Boolean>, TableCell<TableItem, Boolean>>() {
+			@Override
+			public TableCell<TableItem, Boolean> call(TableColumn<TableItem, Boolean> foodItemBoolTableCol) {
+				return new AddItemCell(stage, table);
+			}
+		});
+
 		table.setItems(data);
-		table.getColumns().addAll(nameCol, caloriesCol, fatCol, carbsCol, fiberCol, proteinCol);
+		table.getColumns().addAll(nameCol, caloriesCol, fatCol, carbsCol, fiberCol, proteinCol, actionCol);
 
 		final VBox vbox = new VBox();
 		vbox.setSpacing(3);
@@ -129,6 +163,27 @@ public class FoodTableSample {
 		table.setLayoutX(x + 150);
 		table.setLayoutY(y);
 		return table;
+	}
+
+	// creates the "+/-" button contained in each row of the food item
+	private class AddItemCell extends TableCell<TableItem, Boolean> {
+		final Button addButton = new Button("+/-");
+		final StackPane paddedButton = new StackPane();
+		final DoubleProperty buttonY = new SimpleDoubleProperty();
+
+		AddItemCell(final Stage stage, final TableView table) {
+			paddedButton.setPadding(new Insets(3));
+			paddedButton.getChildren().add(addButton);
+			addButton.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent mouseEvent) {
+					buttonY.set(mouseEvent.getScreenY());
+					;
+				}
+			});
+			setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+			setGraphic(paddedButton);
+		}
 	}
 
 	/**
@@ -145,8 +200,8 @@ public class FoodTableSample {
 		private TableItem(String name, String calories, String fat, String carbs, String fiber, String protein) {
 			this.name = name;
 			this.calories = calories;
-			this.fiber = fiber;
 			this.fat = fat;
+			this.fiber = fiber;
 			this.protein = protein;
 			this.carbs = carbs;
 		}
